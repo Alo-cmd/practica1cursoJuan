29ddbebfe7d87dd0f7cc4c1e1aa923b22b30777e
Jesús Alonso Cornejo Galindo <Jesus@Roundtable.org>
2026-06-30 14:33:46 -0600

    Agregar menu y ajuste de index

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <div class="alert alert-primary mt-4 container text-center">
         <h1>Bienvenido</h1>
         <p>Sistema de gestión para autos</p>
+        <b>Texto en negro</b>
     </div>
     
 </body>
